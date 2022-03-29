8c932fd406e3f1d1c2f7809eecd29cdb0fd57548
Samuel-Brr <samuel.barre972@gmail.com>
2022-03-29 15:37:39 +0200

    dropping this project

--- frontend/src/app/pages/task-view/task-view.component.ts
+++ frontend/src/app/pages/task-view/task-view.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { List } from 'src/app/models/list.model';
 import { Task } from 'src/app/models/task.model';
 import { TaskService } from 'src/app/services/task.service';
@@ -13,26 +13,33 @@ export class TaskViewComponent implements OnInit {
 
   lists: any
   tasks: any
+
+  selectedListId: string = '';
+
   constructor(private taskService: TaskService,
-            private route: ActivatedRoute) { }
+            private route: ActivatedRoute,
+            private router: Router) {
+
+              this.route.params.subscribe(
+                (params: Params) => {
+                  if(params['listId']){
+                    this.taskService.getTasks(params['listId']).subscribe((tasks => {
+                      this.tasks = tasks
+                    }))
+                  }
+                  else this.tasks = undefined
+
+                }
+              )
+
+              this.taskService.getLists().subscribe((lists) => {
+                this.lists = lists
+              })
+
+
+            }
 
   ngOnInit(): void {
-
-    this.route.params.subscribe(
-      (params: Params) => {
-        if(params['listId']){
-          this.taskService.getTasks(params['listId']).subscribe((tasks => {
-            this.tasks = tasks
-          }))
-        }
-        else this.tasks = undefined
-
-      }
-    )
-
-    this.taskService.getLists().subscribe((lists) => {
-      this.lists = lists
-    })
 
   }
 
@@ -40,6 +47,20 @@ export class TaskViewComponent implements OnInit {
     this.taskService.complete(task).subscribe(()=>{
       console.log("completed !")
       task.completed = !task.completed
+    })
+  }
+
+  onDeleteListClick() {
+    this.taskService.deleteList(this.selectedListId).subscribe((res: any) => {
+      this.router.navigate(['lists']);
+      console.log(res);
+    })
+  }
+
+  onDeleteTaskClick(id: string) {
+    this.taskService.deleteTask(this.selectedListId, id).subscribe((res: any) => {
+      this.tasks = this.tasks.filter((val: { _id: string; }) => val._id !== id);
+      console.log(res);
     })
   }
 
--- frontend/src/app/services/task.service.ts
+++ frontend/src/app/services/task.service.ts
@@ -17,6 +17,24 @@ export class TaskService {
     return this.webReqService.post('lists', { title })
   }
 
+  updateList(id: string, title: string) {
+    // We want to send a web request to update a list
+    return this.webReqService.patch(`lists/${id}`, { title });
+  }
+
+  updateTask(listId: string, taskId: string, title: string) {
+    // We want to send a web request to update a list
+    return this.webReqService.patch(`lists/${listId}/task/${taskId}`, { title });
+  }
+
+  deleteTask(listId: string, taskId: string) {
+    return this.webReqService.delete(`lists/${listId}/task/${taskId}`);
+  }
+
+  deleteList(id: string) {
+    return this.webReqService.delete(`lists/${id}`);
+  }
+
   getTasks(listId: string){
     return this.webReqService.get(`lists/${listId}/tasks`)
   }
